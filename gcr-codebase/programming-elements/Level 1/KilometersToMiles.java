301bbda6ab53a7294b6e1bb83f1f15c03ec86658
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-17 13:10:13 +0530

    [Shivam] : updated Kilometers to Miles file

--- gcr-codebase/programming-elements/Level 1/KilometersToMiles.java
+++ gcr-codebase/programming-elements/Level 1/KilometersToMiles.java
@@ -1,20 +1,9 @@
-//Creating a class name with KilometersToMiles indicating to purpose is to convert the distance of Kilometer to miles.
-import java.util.Scanner;
-
 public class KilometersToMiles
 {
 	public static void main(String args[])
 	{	
-		//Create a double variable name with kilometer and Miles.
-		double kilometers, miles;
-
-		//Creating Scanner object to take input from the user.
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Enter the value in Kilometer : ");
-
-		//Assign the user input in the variable.
-		kilometers = input.nextDouble();
+		//Create a double variable name with kilometer and Miles and assign the value 10.8 in Kilometers.
+		double kilometers = 10.8, miles;
 
 		//Multiplication Operator ( * ).
 		//Converting the value of Kilometer to Miles.
@@ -23,7 +12,6 @@ public class KilometersToMiles
 		//Print Miles.
 		System.out.println("The distance " + kilometers + " km in miles is " + miles);
 
-		input.close();
-	}
+		}
 }
 	
