3b13b5009ac319e873ce26dac1a7364092b522bb
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-19 01:22:14 +0530

    [Shivam] : added Day Of Week file

---
+++ gcr-codebase/control-flow/Level 3/DayOfWeek.java
@@ -0,0 +1,35 @@
+//Creating a class name with DayOfWeek indicating to purpose is to prints the day of the week that the date falls on.
+import java.util.Scanner;
+public class DayOfWeek
+{	public static void main(String args[])
+	{	//Create a int vairable name with month, day and year.
+		int month, day, year;
+		String op;
+		//Create Scanner object to take user input.
+		Scanner input = new Scanner(System.in);
+		//Take user input as month, day, year .
+		System.out.println("Enter the month :");
+		month = input.nextInt();
+		System.out.println("Enter the day :");
+		day = input.nextInt();
+		System.out.println("Enter the year :");
+		year = input.nextInt();
+		int yearZ = year - ( 14 - month ) / 12;      			
+		int x = yearZ + ( yearZ / 4 ) - ( yearZ / 100 ) + ( yearZ / 400 );
+		int monthZ = month + 12 * ( ( 14 - month ) / 12 ) - 2;
+		int dayZ = ( day + x + 31 * monthZ / 12 ) % 7;
+
+		//Print Date.
+		System.out.println("The day of the week is: " + dayZ);
+		input.close();
+	}
+}
+
+
+
+
+
+
+
+				
+
