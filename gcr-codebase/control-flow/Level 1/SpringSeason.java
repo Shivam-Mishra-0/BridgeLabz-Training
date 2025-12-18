b02f9fca031cd7e5acfd4c317584918a251c349f
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-18 14:18:59 +0530

    [Shivam] : added Spring Season file

---
+++ gcr-codebase/control-flow/Level 1/SpringSeason.java
@@ -0,0 +1,33 @@
+//Creating a class name with SpringSeason indicating to purpose is to check whether the month is spring or not.
+import java.util.Scanner;
+public class SpringSeason 
+{	public static void main(String args[])
+	{	//Create a int vairable name with month and day.
+		int month, day;
+		
+
+		//Create Scanner object to take user input.
+		Scanner input = new Scanner(System.in);
+		//Take user input as number.
+		System.out.println("Enter the month :");
+		month = input.nextInt();
+		System.out.println("Enter the day :");
+		day = input.nextInt();
+		//Checking the Spring Season using (if-else) loop.
+		boolean isSpringSeason = 
+		(month == 3 && day >= 20 && day <= 31 ) ||
+		(month == 4 && day >= 20 && day <= 31 ) ||
+		(month == 5 && day >= 20 && day <= 31 ) ||
+		(month == 6 && day >= 20 && day <= 31 );
+
+		//Print month and day of Spring Season.
+		if ( isSpringSeason )
+		{	System.out.println("Its a Spring Season");
+		}
+		else
+		{	System.out.println("Not a Spring Season");
+		}		
+		input.close();
+	}
+}
+	
