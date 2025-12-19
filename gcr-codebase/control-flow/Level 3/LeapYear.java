b6dddea831f9e902a612629b610113fe3c18e515
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-19 11:39:55 +0530

    [Shivam] : updated Leap Year file

--- gcr-codebase/control-flow/Level 3/LeapYear.java
+++ gcr-codebase/control-flow/Level 3/LeapYear.java
@@ -12,14 +12,16 @@ public class LeapYear
 		year = input.nextInt();
 		//Check the year is leap year or not.
 		if ( year >= 1582 )
-		{	if ( year % 4 == 0 )
+		{	if ( year % 4 == 0 && year % 100 != 0 )
 			{	System.out.println( year + " is a Leap Year" );
 			}
 			else if ( year % 100 == 0 )
-			{	System.out.println( year + " is not a Leap Year" );
-			}
-			else if ( year % 400 == 0 )
-			{	System.out.println( year + " is a Leap Year" );
+			{	if ( year % 400 == 0 )
+				{	System.out.println( year + " is a Leap Year" );
+				}
+				else
+				{	System.out.println( year + " is not a Leap Year" );
+				}		
 			}
 			else 
 			{	System.out.println( year + " is not a Leap Year" );
