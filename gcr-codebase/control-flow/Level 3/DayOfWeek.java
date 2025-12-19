4a6204d98e1d7f42195840c605a86044954b5c13
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-19 11:00:37 +0530

    [Shivam] : updated Day Of Week file

--- gcr-codebase/control-flow/Level 3/DayOfWeek.java
+++ gcr-codebase/control-flow/Level 3/DayOfWeek.java
@@ -1,11 +1,11 @@
 //Creating a class name with DayOfWeek indicating to purpose is to prints the day of the week that the date falls on.
-import java.util.Scanner;
+//import java.util.Scanner;
 public class DayOfWeek
 {	public static void main(String args[])
 	{	//Create a int vairable name with month, day and year.
 		int month, day, year;
 		String op;
-		//Create Scanner object to take user input.
+		/* //Create Scanner object to take user input.
 		Scanner input = new Scanner(System.in);
 		//Take user input as month, day, year .
 		System.out.println("Enter the month :");
@@ -13,7 +13,13 @@ public class DayOfWeek
 		System.out.println("Enter the day :");
 		day = input.nextInt();
 		System.out.println("Enter the year :");
-		year = input.nextInt();
+		year = input.nextInt(); */
+
+		//With the help of command line arguments.
+		month = Integer.parseInt(args[0]);
+		day = Integer.parseInt(args[1]);
+		year = Integer.parseInt(args[2]);
+		//USing formula to find the day of the week it falls on.
 		int yearZ = year - ( 14 - month ) / 12;      			
 		int x = yearZ + ( yearZ / 4 ) - ( yearZ / 100 ) + ( yearZ / 400 );
 		int monthZ = month + 12 * ( ( 14 - month ) / 12 ) - 2;
@@ -21,9 +27,12 @@ public class DayOfWeek
 
 		//Print Date.
 		System.out.println("The day of the week is: " + dayZ);
-		input.close();
+		//input.close();
 	}
 }
+
+
+ 
 
 
 
