14b14c29fa5902d58472687ab6e964b4981f1ea9
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-23 14:23:51 +0530

    [Shivam] : updated Shortest and Longest file

--- gcr-codebase/strings/Level 2/ShortestAndLongest.java
+++ gcr-codebase/strings/Level 2/ShortestAndLongest.java
@@ -1,26 +1,89 @@
-//Creating a class name with ShortestAndLongest.
+//Creating a class name with ShortestAndLongest .
 import java.util.Scanner;
 public class ShortestAndLongest {
-    public static int findLengthManual(String str) {
-        int count = 0;
-        try {
-            while (true) {
-                str.charAt(count);
+    public static int findLength(String text){
+        int count=0;
+        while(true){
+            try{
+                text.charAt(count);
                 count++;
+            }catch(Exception e){
+                break;
             }
-        } catch (StringIndexOutOfBoundsException e) {
-            return count; 
         }
+        return count;
     }
-	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word: ");
-        String userInput = scanner.next();
-        int manualLen = findLengthManual(userInput);
-        int builtInLen = userInput.length();
+    public static String[] splitText(String text){
+        int len=findLength(text);
+        int wordCount=0;
 
-        System.out.println("Manual Length: " + manualLen);
-        System.out.println("Built-in length(): " + builtInLen);
-        scanner.close();
+        for(int i=0;i<len;i++){
+            if(text.charAt(i)==' ')
+                wordCount++;
+        }
+        wordCount++;
+        String[] words=new String[wordCount];
+        int start=0,index=0;
+
+        for(int i=0;i<=len;i++){
+            if(i==len||text.charAt(i)==' '){
+                String word="";
+                for(int j=start;j<i;j++){
+                    word+=text.charAt(j);
+                }
+                words[index++]=word;
+                start=i+1;
+            }
+        }
+        return words;
+    }
+    public static String[][] createWordLength(String[] words){
+        String[][] data=new String[words.length][2];
+
+        for(int i=0;i<words.length;i++){
+            int length=findLength(words[i]);
+            data[i][0]=words[i];
+            data[i][1]=String.valueOf(length);
+        }
+        return data;
+    }
+    public static int[] findShortestAndLongest(String[][] data){
+        int shortest=0,longest=0;
+
+        for(int i=1;i<data.length;i++){
+            int curr=Integer.parseInt(data[i][1]);
+            int min=Integer.parseInt(data[shortest][1]);
+            int max=Integer.parseInt(data[longest][1]);
+
+            if(curr<min)
+                shortest=i;
+            if(curr>max)
+                longest=i;
+        }
+
+        return new int[]{shortest,longest};
+    }
+
+    public static void main(String[] args){
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the text:");
+        String text=input.nextLine();
+
+        String[] words=splitText(text);
+        String[][] data=createWordLength(words);
+        int[] result=findShortestAndLongest(data);
+
+        System.out.println("\nWords and Lengths:");
+        System.out.println("Word\tLength");
+
+        for(int i=0;i<data.length;i++){
+            System.out.println(data[i][0]+"\t"+Integer.parseInt(data[i][1]));
+        }
+
+        System.out.println("\nShortest Word: "+data[result[0]][0]);
+        System.out.println("Longest Word: "+data[result[1]][0]);
+
+        input.close();
     }
 }
