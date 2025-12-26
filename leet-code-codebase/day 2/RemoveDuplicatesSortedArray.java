7a69337b25cd4baa173767af8505c1382681bbd3
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-26 23:26:38 +0530

    [Shivam] : added Remove Duplicates Sorted Array file

---
+++ leet-code-codebase/day 2/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,37 @@
+
+public class RemoveDuplicatesSortedArray {
+
+    // Method to remove duplicates
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+// index of last unique element
+        int i = 0; 
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; 
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        int[] nums = {0, 0, 1, 1, 2, 2, 3, 3, 4};
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("Number of unique elements" + k);
+        System.out.print("Array after removing duplicates");
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
