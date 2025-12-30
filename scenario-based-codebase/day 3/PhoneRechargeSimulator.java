ce4c6842cddd0288d987975fd443436ae5e06652
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-30 10:27:58 +0530

    [Shivam] : updated Phone Recharge Simulator File

--- scenario-based-codebase/day 3/PhoneRechargeSimulator.java
+++ scenario-based-codebase/day 3/PhoneRechargeSimulator.java
@@ -16,39 +16,44 @@ public class PhoneRechargeSimulator{
         char choice;
 
         do {
-            System.out.print("Enter Mobile Operator (Airtel/Jio/VI) ");
-            String operator = sc.next();
+		System.out.println( "=======================================" );
+            	System.out.println( "|Enter Mobile Operator (Airtel/Jio/VI)|");
+		System.out.println( "=======================================" );
+            	String operator = sc.next();
 
-            System.out.print("Enter Recharge Amount");
-            double amount = sc.nextDouble();
+            	System.out.println("| Enter Recharge Amount           |");
+		System.out.println( "=======================================" );
+            	double amount = sc.nextDouble();
 
             balance += amount;
 
             // switch for offers
             switch (operator.toLowerCase()) {
-                case "airtel":
-                    System.out.println("Offer: 1GB/day for 28 days");
+                case "airtel": 
+		// System.out.println("|  No offers available        |");
+                    System.out.println("|  Offer: 1GB/day for 28 days        |");
                     break;
 
                 case "jio":
-                    System.out.println("Offer: Unlimited calls + 1.5GB/day");
+                    System.out.println("|  Offer: Unlimited calls + 1.5GB/day     |");
                     break;
 
                 case "vi":
-                    System.out.println("Offer: Weekend data rollover");
+                    System.out.println("|  Offer: Weekend data rollover        |");
                     break;
 
                 default:
-                    System.out.println("No offers available");
+                    System.out.println("|  No offers available            |");
             }
-
-            System.out.println("Current Balance: ₹" + balance);
-
-            System.out.print("Do you want to recharge again? (Y/N)");
+	   System.out.println( "=======================================" );
+            System.out.println( "|  Current Balance: ₹        | " + balance  + "   |");
+	  System.out.println( "=======================================" );
+            System.out.println( "| Do you want to recharge again? (Y/N)|" );
             choice = sc.next().charAt(0);
-
+	  System.out.println( "=======================================" );
         } while (choice == 'Y' || choice == 'y');
 
-        System.out.println("Thank you for using Recharge Simulator");
+           System.out.println( "|Thank you for using Recharge Simulator|" );
+	 System.out.println( "=======================================" );
     }
 }
