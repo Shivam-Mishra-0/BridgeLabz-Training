6c097134a2be1fbfaff7d1351775f241732577ac
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:50:10 +0530

    [Shivam] : added University Management Demo file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/UniversityManagementDemo.java
@@ -0,0 +1,15 @@
+package com.objectmodeling;
+
+public class UniversityManagementDemo {
+
+    public static void main(String[] args) {
+
+        StudentNew student = new StudentNew("Rohan");
+        Professor professor = new Professor("Dr. Sharma");
+
+        CourseNew javaCourse = new CourseNew("Java Programming");
+
+        student.enrollCourse(javaCourse);
+        javaCourse.assignProfessor(professor);
+    }
+}
