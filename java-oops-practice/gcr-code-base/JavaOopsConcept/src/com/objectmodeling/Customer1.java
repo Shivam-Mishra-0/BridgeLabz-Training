ca3f88480daea8d284fb0538119f4cee88cf4a5f
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:40:36 +0530

    [Shivam] : added Customer1 file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/Customer1.java
@@ -0,0 +1,14 @@
+package com.objectmodeling;
+
+public class Customer1 {
+    String name;
+
+    public Customer1(String name) {
+        this.name = name;
+    }
+
+    public void placeOrder(Order order) {
+        System.out.println(name + " placed an order:");
+        order.showOrderDetails();
+    }
+}
