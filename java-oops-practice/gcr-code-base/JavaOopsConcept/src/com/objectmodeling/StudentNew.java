fe0a65ed646630ca1c117e2f5cfbad65f22f9af7
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:48:19 +0530

    [Shivam] : added Studenet New file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/StudentNew.java
@@ -0,0 +1,19 @@
+package com.objectmodeling;
+import java.util.ArrayList;
+
+public class StudentNew {
+
+    String name;
+    ArrayList<CourseNew> enrolledCourses;
+
+    public StudentNew(String name) {
+        this.name = name;
+        enrolledCourses = new ArrayList<>();
+    }
+
+    // Student communicates with Course
+    public void enrollCourse(CourseNew course) {
+        enrolledCourses.add(course);
+        System.out.println(name + " enrolled in " + course.courseName);
+    }
+}
