2330e58a3740112deb22dab2366f2938f8dadd98
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:47:09 +0530

    [Shivam] : added School Demo file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/SchoolDemo.java
@@ -0,0 +1,23 @@
+package com.objectmodeling;
+
+public class SchoolDemo {
+    public static void main(String[] args) {
+        School school = new School("Green Valley School");
+
+        Student rohan = new Student("Rohan");
+        Student neha = new Student("Neha");
+
+        Course math = new Course("Mathematics");
+        Course science = new Course("Science");
+
+        school.addStudent(rohan);
+        school.addStudent(neha);
+
+        rohan.enrollCourse(math);
+        rohan.enrollCourse(science);
+        neha.enrollCourse(science);
+
+        rohan.viewCourses();
+        science.showStudents();
+    }
+}
