a5f861629c41c3d0723ed1e34097341d6f5a3656
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:41:47 +0530

    [Shivam] : added Doctor file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/Doctor.java
@@ -0,0 +1,15 @@
+package com.objectmodeling;
+
+
+public class Doctor {
+    String name;
+
+    public Doctor(String name) {
+        this.name = name;
+    }
+
+    public void consult(Patient patient) {
+        System.out.println("Dr. " + name +
+                " is consulting patient " + patient.name);
+    }
+}
