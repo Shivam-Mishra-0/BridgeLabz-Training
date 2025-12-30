af9cba9e6c0d60ba809aa9f820871a929dd2c7b3
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:46:47 +0530

    [Shivam] : added School file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/School.java
@@ -0,0 +1,16 @@
+package com.objectmodeling;
+
+import java.util.ArrayList;
+
+public class School {
+    String schoolName;
+    ArrayList<Student> students = new ArrayList<>();
+
+    public School(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+}
