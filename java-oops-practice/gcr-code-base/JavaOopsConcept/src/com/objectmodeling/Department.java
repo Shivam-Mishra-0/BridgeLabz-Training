cf16674f0ae4e7f41d20cc36dce50811673b3c25
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:41:06 +0530

    [Shivam] : added Department file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/Department.java
@@ -0,0 +1,18 @@
+package com.objectmodeling;
+
+import java.util.ArrayList;
+
+public class Department {
+
+    String departmentName;
+    ArrayList<Employee> employees;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+        employees = new ArrayList<>();
+    }
+
+    public void addEmployee(String employeeName) {
+        employees.add(new Employee(employeeName));
+    }
+}
