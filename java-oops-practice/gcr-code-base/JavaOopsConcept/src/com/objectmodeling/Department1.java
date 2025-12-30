0d2da99735281995d7fd1fa121287e3df548367b
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:41:22 +0530

    [Shivam] : added Department1 file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/Department1.java
@@ -0,0 +1,9 @@
+package com.objectmodeling;
+public class Department1 {
+
+    String departmentName;
+
+    public Department1(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
