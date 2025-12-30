b25871f11a6b07675d322f82f8b20787cb409cff
HackHeal-1828191 <cy.shivam.m@gmail.com>
2025-12-31 00:39:03 +0530

    [Shivam] : added Company Demo file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodeling/CompanyDemo.java
@@ -0,0 +1,22 @@
+package com.objectmodeling;
+
+public class CompanyDemo {
+
+    public static void main(String[] args) {
+
+        Company techCompany = new Company("TechSoft");
+
+        Department dev = new Department("Development");
+        dev.addEmployee("Aman");
+        dev.addEmployee("Ravi");
+
+        Department hr = new Department("HR");
+        hr.addEmployee("Neha");
+
+        techCompany.addDepartment(dev);
+        techCompany.addDepartment(hr);
+
+        // Closing company
+        techCompany.closeCompany();
+    }
+}
