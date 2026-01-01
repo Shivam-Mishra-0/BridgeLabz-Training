1a5f020ed21461f8241191211ef261ca1af9aae0
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-01 16:37:41 +0530

    [Shivam] : added Full Time Employee class file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/encapsulation/employeemanagement/FullTimeEmployee.java
@@ -0,0 +1,25 @@
+package com.encapsulation.employeemanagement;
+
+public class FullTimeEmployee extends Employee implements Department {
+
+    private String department;
+
+    public FullTimeEmployee(int id, String name, double baseSalary) {
+        super(id, name, baseSalary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary(); // fixed salary
+    }
+
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+}
