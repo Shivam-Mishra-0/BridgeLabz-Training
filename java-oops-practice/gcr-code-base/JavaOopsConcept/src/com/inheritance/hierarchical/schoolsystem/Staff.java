5f612efa9d9037461465c533fae3e546fe34b9f0
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-01 12:34:41 +0530

    [Shivam] : added Staff class file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/inheritance/hierarchical/schoolsystem/Staff.java
@@ -0,0 +1,21 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Staff extends Person {
+
+    private String department;
+
+    // Constructor to initialize staff details
+    public Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    // Overridden method to describe staff role
+    @Override
+    public void displayRole() {
+    	//System.out.println("\033[70G|");
+        System.out.println("| Role        | Staff            |");
+        System.out.println("| Department  | " + department);
+        System.out.println("+================================+");
+    }
+}
