cdd7d3a5117f2579c39038016920b83c40e97984
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-01 12:34:57 +0530

    [Shivam] : added Student class file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/inheritance/hierarchical/schoolsystem/Student.java
@@ -0,0 +1,20 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Student extends Person {
+
+    private String grade;
+
+    // Constructor to initialize student details
+    public Student(String name, int age, String grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    // Overridden method to describe student role
+    @Override
+    public void displayRole() {
+    	//System.out.println("\033[A\033[33C|");
+        System.out.println("| Role        | Student          |");
+        System.out.println("| Grade       | " + grade);
+    }
+}
