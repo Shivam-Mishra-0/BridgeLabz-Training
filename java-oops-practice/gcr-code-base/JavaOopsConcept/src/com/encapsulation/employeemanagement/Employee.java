fb95f7b7afce86d30abe5d62986c2cf8d0f258fe
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-01 16:33:34 +0530

    [Shivam] : added Employee class file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/encapsulation/employeemanagement/Employee.java
@@ -0,0 +1,50 @@
+package com.encapsulation.employeemanagement;
+
+public abstract class Employee {
+
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    // Constructor
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    // Getters & Setters (Encapsulation)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    // Abstract method
+    public abstract double calculateSalary();
+
+    // Concrete method
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + calculateSalary());
+    }
+}
