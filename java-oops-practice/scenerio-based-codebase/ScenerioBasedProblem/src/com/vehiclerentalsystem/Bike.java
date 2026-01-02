f77df2a7b1cf2f3fc36c43a0b260fce6ae4be1c2
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-02 20:17:27 +0530

    [Shivam] : added Bike class file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/vehiclerentalsystem/Bike.java
@@ -0,0 +1,14 @@
+package com.vehiclerentalsystem;
+
+public class Bike extends Vehicle implements Rentable {
+
+    public Bike(int vehicleId, String brand, double baseRate) {
+        super(vehicleId, brand, baseRate);
+    }
+
+    @Override
+    public double calculateRent(int days) {
+        return baseRate * days;
+    }
+}
+
