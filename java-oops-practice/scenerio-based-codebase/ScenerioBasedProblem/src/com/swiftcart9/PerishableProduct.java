aebbab0cc7a297259de54b7b9a18647be97e5efc
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-06 10:39:03 +0530

    [Shivam] : added Perishable Product class file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/swiftcart9/PerishableProduct.java
@@ -0,0 +1,13 @@
+package com.swiftcart9;
+
+public class PerishableProduct extends Product {
+
+    PerishableProduct(String name, double price, String category) {
+        super(name, price, category);
+    }
+
+    @Override
+    public double applyDiscount(double totalPrice) {
+        return totalPrice * 0.10;
+    }
+}
