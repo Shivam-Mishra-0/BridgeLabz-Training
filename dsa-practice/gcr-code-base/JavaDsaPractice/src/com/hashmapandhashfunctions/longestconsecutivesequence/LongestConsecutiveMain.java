5945b7d60973bdf20a2a652b3854735bced77cdd
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-01-07 22:28:29 +0530

    [Shivam] : updated longestconsecutivesequence file

--- dsa-practice/gcr-code-base/JavaDsaPractice/src/com/hashmapandhashfunctions/longestconsecutivesequence/LongestConsecutiveMain.java
+++ dsa-practice/gcr-code-base/JavaDsaPractice/src/com/hashmapandhashfunctions/longestconsecutivesequence/LongestConsecutiveMain.java
@@ -1,6 +1,6 @@
 package com.hashmapandhashfunctions.longestconsecutivesequence;
 public class LongestConsecutiveMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         int[] nums = {100, 4, 200, 1, 3, 2};
 
