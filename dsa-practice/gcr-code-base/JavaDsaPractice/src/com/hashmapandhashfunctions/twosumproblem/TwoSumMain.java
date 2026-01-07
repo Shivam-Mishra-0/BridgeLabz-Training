d43d9be8308cf42e451c7b4e76fee31b79d8d160
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-01-07 22:30:30 +0530

    [Shivam] : updated Two Sum main file

--- dsa-practice/gcr-code-base/JavaDsaPractice/src/com/hashmapandhashfunctions/twosumproblem/TwoSumMain.java
+++ dsa-practice/gcr-code-base/JavaDsaPractice/src/com/hashmapandhashfunctions/twosumproblem/TwoSumMain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class TwoSumMain {
     public static void main(String[] args) {
 
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 7, 11, 15}; 
         int target = 9;
 
         int[] result = TwoSum.findTwoSum(nums, target);
