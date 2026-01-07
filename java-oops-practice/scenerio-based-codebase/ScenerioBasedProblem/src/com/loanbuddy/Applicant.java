0d051b2a4c9bad61546170c8e9e261abe413bc64
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-07 18:12:35 +0530

    [Shivam] : added Applicant file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/loanbuddy/Applicant.java
@@ -0,0 +1,52 @@
+/*
+14. "LoanBuddy – Loan Approval Automation"
+Story: A fintech startup named FinlyTech is launching a product called LoanBuddy, a web and
+mobile app that automates personal loan approvals for salaried and self-employed applicants.
+LoanBuddy should collect applicant data, verify eligibility, evaluate credit risk, and either
+approve or reject the loan based on custom rules. It also calculates monthly EMIs and provides
+a personalized repayment plan.
+You’re hired as a Java backend developer to design and implement the core loan approval
+engine.
+Requirements:
+● Applicant class: name, creditScore, income, loanAmount.
+● LoanApplication class with loan type, term, and interest rate.
+● Interface IApprovable with approveLoan() and calculateEMI().
+● Use constructors to support different types of loans (personal, home, auto).
+● Operators for EMI calculation: P × R × (1+R)^N / ((1+R)^N – 1)
+● Encapsulation: keep credit score and internal approval logic private.
+● Inheritance: different loan types (HomeLoan, AutoLoan) extending base class.
+● Polymorphism: EMI calculation logic varies by loan type.
+● Access modifiers to limit loan status changes to internal processes only.
+ */
+
+package com.loanbuddy;
+
+public class Applicant 
+{	private String name;
+	private double income;
+	private double loanAmount;
+	private int creditScore;
+	
+	public Applicant ( String name, double income, double loanAmount, int creditScore )
+	{	this.name = name;
+		this.income = income;
+		this.loanAmount = loanAmount;
+		this.creditScore = creditScore;
+	}
+	
+	int getCreditScore()
+	{	return creditScore;
+	}
+	
+	public double getIncome()
+	{	return income;		
+	}
+	
+	public double getLoanAmount()
+	{	return loanAmount;		
+	}
+	
+	public String getName()
+	{	return name;		
+	}
+}
