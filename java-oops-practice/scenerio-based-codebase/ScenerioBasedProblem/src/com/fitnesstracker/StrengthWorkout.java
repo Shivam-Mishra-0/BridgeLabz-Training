41f5001f1d44e6217118c5a16128c905ff2f8df9
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-07 11:08:11 +0530

    [Shivam] : added Fitness Tracker file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/fitnesstracker/StrengthWorkout.java
@@ -0,0 +1,14 @@
+package com.fitnesstracker;
+
+public class StrengthWorkout extends Workout {
+
+    public StrengthWorkout(int duration) {
+        super("Strength", duration);
+    }
+
+    @Override
+    public void calculateCalories() {
+        // Strength burns fewer calories per minute
+        caloriesBurned = duration * 5.5;
+    }
+}
