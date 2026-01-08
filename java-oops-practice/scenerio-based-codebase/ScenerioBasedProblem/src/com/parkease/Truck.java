96d455a37c3d20cb763d076e6c7fd4a20df91226
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-08 11:23:06 +0530

    [Shivam] : added Truck file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/parkease/Truck.java
@@ -0,0 +1,15 @@
+package com.parkease;
+
+public class Truck extends Vehicle {
+    public Truck(String vehicleNumber) {
+        super(vehicleNumber);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double baseRate = 100;
+        double penalty = (hours > 4) ? 200 : 0;
+
+        return (baseRate * hours) + penalty;
+    }
+}
