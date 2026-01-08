b29dc7a0e0f6b84bbd7032caec78356e29da0bfe
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-08 11:23:24 +0530

    [Shivam] : added Vehicle file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/parkease/Vehicle.java
@@ -0,0 +1,9 @@
+package com.parkease;
+
+public abstract class Vehicle implements IPayable {
+    protected String vehicleNumber;
+
+    public Vehicle(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+}
