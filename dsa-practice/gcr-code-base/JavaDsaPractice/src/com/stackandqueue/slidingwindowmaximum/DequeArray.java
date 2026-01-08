c03e29a7ceb53583264880ea9d4c7926c50d12a6
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-01-08 09:49:38 +0530

    [Shivam] : Update DequeArray.java file

--- dsa-practice/gcr-code-base/JavaDsaPractice/src/com/stackandqueue/slidingwindowmaximum/DequeArray.java
+++ dsa-practice/gcr-code-base/JavaDsaPractice/src/com/stackandqueue/slidingwindowmaximum/DequeArray.java
@@ -2,7 +2,7 @@ package com.stackandqueue.slidingwindowmaximum;
 
 public class DequeArray {
 
-    private int[] elements;
+    private int[] elements; 
     private int front;
     private int rear;
 
