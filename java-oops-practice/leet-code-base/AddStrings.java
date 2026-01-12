cc40c9423bd55e729cb408f6660a50fc457998f8
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-12 12:07:05 +0530

    [Shivam] : updated Leetcode add strings file

--- java-oops-practice/leet-code-base/AddStrings.java
+++ java-oops-practice/leet-code-base/AddStrings.java
@@ -1,33 +1,42 @@
+
 import java.util.Scanner;
 
-public class NumberOfSegmentsInAString {
+public class AddStrings {
 
-    // Counts number of words in a string
-    public int countSegments(String s) {
-        int count = 0;
-        boolean inWord = false;
+    // Adds two numbers represented as strings
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        String result = "";
 
-        // Traverse characters to find word boundaries
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ' ' && !inWord) {
-                count++;
-                inWord = true;
-            } else if (s.charAt(i) == ' ') {
-                inWord = false;
-            }
+        // Add digits from right to left
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+
+            if (i >= 0)
+                sum += num1.charAt(i--) - '0';
+            if (j >= 0)
+                sum += num2.charAt(j--) - '0';
+
+            result = (sum % 10) + result;
+            carry = sum / 10;
         }
 
-        return count;
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        LC0434_NumberOfSegmentsInAString sol = new LC0434_NumberOfSegmentsInAString();
+        LC0415_AddStrings sol = new LC0415_AddStrings();
 
-        System.out.print("Enter a sentence: ");
-        String s = input.nextLine();
+        System.out.print("Enter first number string: ");
+        String num1 = input.nextLine();
 
-        System.out.println(sol.countSegments(s));
+        System.out.print("Enter second number string: ");
+        String num2 = input.nextLine();
+
+        System.out.println(sol.addStrings(num1, num2));
 
         input.close();
     }
