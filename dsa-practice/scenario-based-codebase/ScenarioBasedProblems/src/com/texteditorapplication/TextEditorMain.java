35828fbcd7cff0270aaa722184178bfcd00c18e3
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-15 22:44:30 +0530

    [Shivam] : added Text Editor main file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/texteditorapplication/TextEditorMain.java
@@ -0,0 +1,24 @@
+package com.texteditorapplication;
+
+public class TextEditorMain{
+    public static void main(String[] args) {
+
+        TextEditor editor = new TextEditor();
+
+        editor.insert("Hello ");
+        editor.insert("World");
+        editor.display();
+
+        editor.undo();
+        editor.display();
+
+        editor.redo();
+        editor.display();
+
+        editor.delete(5);
+        editor.display();
+
+        editor.undo();
+        editor.display();
+    }
+}
