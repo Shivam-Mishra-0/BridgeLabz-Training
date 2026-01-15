8f55acfb57d8ca6d7c1cfe8be548d2850cd138b1
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-15 22:45:48 +0530

    [Shivam] : added Vehicle file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/trafficmanagerapplication/Vehicle.java
@@ -0,0 +1,12 @@
+package com.trafficmanagerapplication;
+
+public class Vehicle {
+	String number;
+    Vehicle next;
+
+    public Vehicle(String number) {
+        this.number = number;
+        this.next = null;
+    }
+
+}
