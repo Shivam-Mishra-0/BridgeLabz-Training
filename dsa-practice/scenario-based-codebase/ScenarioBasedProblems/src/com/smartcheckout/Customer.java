56418e46415b8b453e073410c7abcb9d16b27cad
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-16 11:34:02 +0530

    [Shivam] : added Customer file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/smartcheckout/Customer.java
@@ -0,0 +1,12 @@
+package com.smartcheckout;
+
+import java.util.*;
+public class Customer {
+    String  name;
+    List<String>items;
+	public Customer( String  name, List<String>items) {
+	this.name=name;
+	this.items=items;
+	}
+
+}
