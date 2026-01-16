cf166d25aa26220dce6db6e500c9198fd751acf9
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-16 23:19:32 +0530

    [Shivam] : added water bill file

---
+++ java-oops-practice/scenerio-based-codebase/ScenerioBasedProblem/src/com/payxpress/WaterBill.java
@@ -0,0 +1,16 @@
+package com.payxpress;
+
+import java.time.LocalDate; 
+class WaterBill extends Bill {
+
+    public WaterBill(double amount, LocalDate dueDate) {
+        super("Water", amount, dueDate);
+    }
+
+    @Override
+    public void sendReminder() {
+        if (!isPaid()) {
+            System.out.println("💧 Water Bill Reminder: Please pay to avoid penalty.");
+        }
+    }
+}
