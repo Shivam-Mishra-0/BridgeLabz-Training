73fd6c0a3bbe62039bfb11320e2952f37cebce43
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-18 18:05:26 +0530

    [Shivam] : added Package file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/robowarehouseapplication/Package.java
@@ -0,0 +1,27 @@
+
+package com.robowarehouseapplication;
+
+public class Package {
+	private String id;
+    private double weight;
+    
+    // constructor
+    public Package(String id, double weight) {
+    	this.id = id;
+    	this.weight = weight;
+    }
+
+	public String getId() {
+		return id;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+	
+	// Good string representation
+	@Override
+	public String toString() {
+        return id + " (" + weight + " kg)";
+    }
+}
