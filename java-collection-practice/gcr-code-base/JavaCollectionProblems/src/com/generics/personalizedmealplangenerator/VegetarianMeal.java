151f34d95c50ef8c3d2dc9dce8faea12ffd75c3c
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-19 11:44:38 +0530

    [Shivam] : added VegetarianMeal file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/generics/personalizedmealplangenerator/VegetarianMeal.java
@@ -0,0 +1,15 @@
+
+package com.generics.personalizedmealplangenerator;
+
+public class VegetarianMeal implements MealPlan {
+
+    @Override
+    public String getCategory() {
+        return "Vegetarian";
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("Meal: Paneer, Vegetables, Rice");
+    }
+}
