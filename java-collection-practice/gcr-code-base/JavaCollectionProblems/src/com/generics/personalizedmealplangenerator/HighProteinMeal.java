f0305077c314d802efd4be0806f89933645c3ff1
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-19 11:41:36 +0530

    [Shivam] : added HighProteinMeal file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/generics/personalizedmealplangenerator/HighProteinMeal.java
@@ -0,0 +1,16 @@
+
+package com.generics.personalizedmealplangenerator;
+
+public class HighProteinMeal implements MealPlan {
+
+    @Override
+    public String getCategory() {
+        return "High Protein";
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("Meal: Chicken, Beans, Nuts");
+    }
+}
+
