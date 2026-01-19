513b34d05f6a6b52ef776b9f27d818d4720ba3e2
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-19 11:22:18 +0530

    [Shivam] : added ProductManager file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/generics/aidrivenresumescreeningsystem/ProductManager.java
@@ -0,0 +1,15 @@
+
+package com.generics.aidrivenresumescreeningsystem;
+
+public class ProductManager extends JobRole {
+
+    public ProductManager(String candidateName) {
+        super(candidateName);
+    }
+
+    @Override
+    public void evaluateSkills() {
+        System.out.println(candidateName + " evaluated for leadership and communication skills.");
+    }
+}
+
