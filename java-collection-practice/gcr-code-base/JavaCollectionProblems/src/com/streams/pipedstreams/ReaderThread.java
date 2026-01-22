6c2c968cc9c4f511d1eeff2429cf798e1e1e622c
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-22 11:32:03 +0530

    [Shivam] : added ReaderThread file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/streams/pipedstreams/ReaderThread.java
@@ -0,0 +1,29 @@
+package com.streams.pipedstreams;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+
+public class ReaderThread extends Thread {
+	private PipedInputStream inputStream;
+
+	// constructor
+	public ReaderThread(PipedInputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	@Override
+	public void run() {
+		try {
+			int data;
+
+			while ((data = inputStream.read()) != -1) {
+				System.out.print((char) data);
+			}
+
+			inputStream.close();
+
+		} catch (IOException e) {
+			System.out.println("Reader Error: " + e.getMessage());
+		}
+	}
+}
