fa0b696a36836c47d60551938ef8303c438e1f95
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-23 23:07:36 +0530

    [Shivam] : added Stall file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/foodfest/Stall.java
@@ -0,0 +1,11 @@
+package com.day8.foodfest;
+
+public class Stall {
+    String stallName;
+    int footfall;
+
+    Stall(String stallName, int footfall) {
+        this.stallName = stallName;
+        this.footfall = footfall;
+    }
+}
