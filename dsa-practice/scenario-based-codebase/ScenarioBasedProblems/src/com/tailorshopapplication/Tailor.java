c01be4dc254948b81217e3f2a0b9b0636d6feb6e
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-23 23:01:55 +0530

    [Shivam] : added Tailor file

---
+++ dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/tailorshopapplication/Tailor.java
@@ -0,0 +1,11 @@
+package com.tailorshopapplication;
+
+public class Tailor {
+	String clothName;
+	int deadlineDate;
+	
+	public Tailor(String clothName, int deadlineDate) {
+		this.clothName = clothName;
+		this.deadlineDate = deadlineDate;
+	}
+}
