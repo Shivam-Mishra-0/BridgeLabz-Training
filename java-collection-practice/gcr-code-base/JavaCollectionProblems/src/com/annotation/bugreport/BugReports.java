48fcc6485af35d6e2a3d1c2054d18d52cf2d21cc
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-26 23:19:15 +0530

    [Shivam] : added BugReports file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/annotation/bugreport/BugReports.java
@@ -0,0 +1,15 @@
+
+package com.annotation.bugreport;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+// Container annotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BugReports {
+
+    BugReport[] value();
+}
