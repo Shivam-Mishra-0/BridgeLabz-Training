57c2d28363f37e48919d29bf0a6cbbd0db497fa9
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-27 15:10:55 +0530

    [Shivam] : updated NumberUtilsTest file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionProblems/src/com/junit/test/NumberUtilsTest.java
@@ -0,0 +1,40 @@
+package com.junit.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.junit.main.NumberUtils;
+
+public class NumberUtilsTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    public void testIsEvenWithEvenNumbers(int number) {
+        assertTrue(NumberUtils.isEven(number), number + " should be even");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})
+    public void testIsEvenWithOddNumbers(int number) {
+        assertFalse(NumberUtils.isEven(number), number + " should be odd");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "2, true",
+        "4, true",
+        "6, true",
+        "7, false",
+        "9, false"
+    })
+    public void testIsEvenWithCsvSource(int number, boolean expected) {
+        assertEquals(expected, NumberUtils.isEven(number));
+    }
+}
+
