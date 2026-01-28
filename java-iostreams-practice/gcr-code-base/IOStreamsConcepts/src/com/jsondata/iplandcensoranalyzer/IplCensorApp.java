5122dc2042f0edb1fbec8e182cbf0d2fd4dfa9bd
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-28 16:52:44 +0530

    [Shivam] : updated IplCensorApp file

--- java-iostreams-practice/gcr-code-base/IOStreamsConcepts/src/com/jsondata/iplandcensoranalyzer/IplCensorApp.java
+++ java-iostreams-practice/gcr-code-base/IOStreamsConcepts/src/com/jsondata/iplandcensoranalyzer/IplCensorApp.java
@@ -1,10 +1,10 @@
-package com.jsondatahandling.iplandcensoranalyzer;
+package com.jsondata.iplandcensoranalyzer;
 
 public class IplCensorApp {
     public static void main(String[] args) {
         try {
-            JsonIplProcessor.processJson("src/com/jsondatahandling/resources/ipl.json", "src/com/jsondatahandling/resources/ipl_censored.json");
-            CsvIplProcessor.processCsv("src/com/jsondatahandling/resources/ipl.csv","src/com/jsondatahandling/resources/ipl_censored.csv");
+            JsonIplProcessor.processJson("src/com/jsondatahandling/resources/ipl.json", "src/com/jsondata/resources/ipl_censored.json");
+            CsvIplProcessor.processCsv("src/com/jsondatahandling/resources/ipl.csv","src/com/jsondata/resources/ipl_censored.csv");
 
             System.out.println("Censorship completed!");
 
