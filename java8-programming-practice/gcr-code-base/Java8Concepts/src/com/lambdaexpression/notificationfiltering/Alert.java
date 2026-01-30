fcd0b2928253ba60519edb8d521e19d2ef9d1bb4
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-30 16:43:23 +0530

    [Shivam] : added Alert file

---
+++ java8-programming-practice/gcr-code-base/Java8Concepts/src/com/lambdaexpression/notificationfiltering/Alert.java
@@ -0,0 +1,21 @@
+package com.lambdaexpression.notificationfiltering;
+
+public class Alert {
+    String message;
+    String type; 
+
+    Alert(String message, String type) {
+    	
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return message + " (" + type + ")";
+    }
+}
