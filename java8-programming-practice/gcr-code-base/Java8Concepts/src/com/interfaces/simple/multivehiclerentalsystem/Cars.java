2508021aa83daa53c8c5c94eaef32f8ba786bea6
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-01-30 16:06:56 +0530

    [Shivam] : added Cars file

---
+++ java8-programming-practice/gcr-code-base/Java8Concepts/src/com/interfaces/simple/multivehiclerentalsystem/Cars.java
@@ -0,0 +1,28 @@
+package com.interfaces.simple.multivehiclerentalsystem;
+
+public class Cars  implements RentalSystem {
+
+	
+	private Boolean status;
+	
+	public Cars() {
+		this.status = false;
+	}
+	
+	@Override
+	public void rent() {
+		// TODO Auto-generated method stub
+		status = true;
+		System.out.println("Car Rentad Successfully");
+	}
+
+	@Override
+	public void returnVehicle() {
+		// TODO Auto-generated method stub
+		status = true;
+		
+		
+		System.out.println("Car Returned Successfully\n");
+	}
+
+}
