dcf6a2e80c20a823a18d44279de5310da834c4fe
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-03 12:24:47 +0530

    [Shivam] : added Product file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioJava8Problems/src/com/functionalinterface/comparator/Product.java
@@ -0,0 +1,16 @@
+package com.functionalinterface.comparator;
+
+public class Product 
+{
+
+    private double price;
+
+    public Product(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
