d4556aeefd8c696dc8af7da3ade4ca7a3d5224f1
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-03 12:25:02 +0530

    [Shivam] : added Student file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioJava8Problems/src/com/functionalinterface/comparator/Student.java
@@ -0,0 +1,16 @@
+package com.functionalinterface.comparator;
+
+public class Student 
+{
+
+    private int rank;
+
+    public Student(int rank) {
+        this.rank = rank;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+}
+
