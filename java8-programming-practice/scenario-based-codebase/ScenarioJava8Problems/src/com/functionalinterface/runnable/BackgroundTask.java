eb24b9c8c33d532c4a14cf2075ceec13a367211b
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-03 12:35:55 +0530

    [Shivam] : added BackgroundTask file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioJava8Problems/src/com/functionalinterface/runnable/BackgroundTask.java
@@ -0,0 +1,10 @@
+package com.functionalinterface.runnable;
+
+public class BackgroundTask implements Runnable 
+{
+
+    @Override
+    public void run() {
+        System.out.println("Background task running");
+    }
+}
