8c3acac1adf7bd8280544290ecbc1a51ffeb2e74
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-03 12:37:26 +0530

    [Shivam] : added StudentIdGenerator file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioJava8Problems/src/com/functionalinterface/supplier/StudentIdGenerator.java
@@ -0,0 +1,12 @@
+package com.functionalinterface.supplier;
+
+import java.util.Random;
+
+public class StudentIdGenerator 
+{
+
+    public static int generate() {
+        return new Random().nextInt(100000);
+    }
+}
+
