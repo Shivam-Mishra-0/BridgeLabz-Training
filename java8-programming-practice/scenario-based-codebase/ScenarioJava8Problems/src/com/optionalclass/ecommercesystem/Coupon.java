4154d38470f1a1f89b14823d24c4375465e12b42
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-04 10:07:59 +0530

    [Shivam] : added ecommercesystem file

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioJava8Problems/src/com/optionalclass/ecommercesystem/Coupon.java
@@ -0,0 +1,16 @@
+package com.optionalclass.ecommercesystem;
+
+public class Coupon 
+{
+
+    private String code;
+
+    public Coupon(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
+
