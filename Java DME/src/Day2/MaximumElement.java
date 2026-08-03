a57a8a5c4a1d30aad54a532e33151712b0ab921c
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-03 22:02:11 +0530

    [Shivam] : Day 2 - Updated MaximumElement problem of Hackerrank is completed.

--- Java DME/src/Day2/MaximumElement.java
+++ Java DME/src/Day2/MaximumElement.java
@@ -1,18 +1,9 @@
 package Day2;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
-class Result {
+
+class MaximumElement {
 
 
     public static List<Integer> getMax(List<String> operations) {
@@ -50,32 +41,3 @@ class Result {
 
 }
 
-public class MaximumElement {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<String> ops = IntStream.range(0, n).mapToObj(i -> {
-                    try {
-                        return bufferedReader.readLine();
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                })
-                .collect(toList());
-
-        List<Integer> res = Result.getMax(ops);
-
-        bufferedWriter.write(
-                res.stream()
-                        .map(Object::toString)
-                        .collect(joining("\n"))
-                        + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-}
