75ccb4f77195ba30c76db8f6c25f105ea517f658
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-05 23:16:22 +0530

    [Shivam] : Day 4 - BubbleSort, practice problem is completed.

---
+++ Java DME/src/Day4/BubbleSort.java
@@ -0,0 +1,31 @@
+package Day4;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++ ){
+            boolean swapped = false;
+            for( int j = 0; j < n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 2, 13, 45, 6, 77, 0};
+
+        bubbleSort(arr);
+        for( int value: arr){
+            System.out.print(value + " -> ");
+        }
+    }
+}
