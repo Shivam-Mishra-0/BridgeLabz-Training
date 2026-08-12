5cebff959b2320fcf7c59ac454a2dc04fdff96d9
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-12 09:47:46 +0530

    [Shivam] : Day 8 - MutualRecursion, Practice problem is completed.

---
+++ Java DME/src/Day8/classwork/MutualRecursion.java
@@ -0,0 +1,17 @@
+package Day8.classwork;
+
+public class MutualRecursion {
+    public static boolean isEven(int n) {
+        if(n==0)    return true;
+        return isOdd(n-1);
+    }
+    public static boolean isOdd(int n){
+        if(n==0)    return false;
+        return isEven(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4));
+        System.out.println(isOdd(7));
+    }
+}
