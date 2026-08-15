69bc761b83dd00ddcc5853e98a068358e5c3ef12
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-15 22:10:43 +0530

    [Shivam] : Day 11 - StringMethod, classwork problem is completed.

---
+++ Java DME/src/Day11/classwork/StringMethod.java
@@ -0,0 +1,10 @@
+package Day11.classwork;
+
+public class StringMethod {
+
+    public static void main(String[] args) {
+        //reverse method
+        StringBuilder sb = new StringBuilder("Shivam");
+        System.out.println(sb.reverse().toString());
+    }
+}
