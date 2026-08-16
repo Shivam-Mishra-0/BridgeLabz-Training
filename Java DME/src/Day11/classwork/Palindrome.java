422900362154f728fc9bc674fe2c05b1e7ea0239
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-16 23:00:56 +0530

    [Shivam] : Day 11 - Palindrome, classwork problem is completed.

---
+++ Java DME/src/Day11/classwork/Palindrome.java
@@ -0,0 +1,20 @@
+package Day11.classwork;
+
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "mom";
+        System.out.println(isPalindrome(s));
+    }
+}
