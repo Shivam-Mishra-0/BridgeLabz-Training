7b72b1522e7e8443328191d95fdab8548ea9b5e7
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-16 22:30:52 +0530

    [Shivam] : Day 11 - AlternatingCharacter, HackerRank problem is completed.

---
+++ Java DME/src/Day11/HackerRank/AlternatingCharacter.java
@@ -0,0 +1,43 @@
+package Day11.HackerRank;
+
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+
+class Result29 {
+    public static int alternatingCharacters(String s) {
+        int deletions = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                deletions++;
+            }
+        }
+        return deletions;
+    }
+
+}
+
+public class AlternatingCharacter {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result29.alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
