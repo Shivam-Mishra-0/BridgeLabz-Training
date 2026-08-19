453ed7b7e0148b1c756df394ee1c0f2c707a6f37
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-19 23:14:39 +0530

    [Shivam] : Day 14 - MethodOverridingII, HackerRank problem is completed.

---
+++ Java DME/src/Day14/HackerRank/MethodOverridingII.java
@@ -0,0 +1,32 @@
+package Day14.HackerRank;
+
+import java.util.*;
+import java.io.*;
+
+
+class BiCycle{
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle{
+    String define_me(){
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle(){
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+        BiCycle b = new BiCycle();
+        String temp=b.define_me(); //Fix this line
+
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+
+}
+class MethodOverridingII{
+    public static void main(String []args){
+        MotorCycle M=new MotorCycle();
+    }
+}
+
