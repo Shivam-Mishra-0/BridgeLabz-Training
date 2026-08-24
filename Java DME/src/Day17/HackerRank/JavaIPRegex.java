da6103e811239461e7084e862167c76e56d0c864
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-24 22:36:23 +0530

    [Shivam] : Day17 - JavaIPRegex, HackerRank problem is completed

---
+++ Java DME/src/Day17/HackerRank/JavaIPRegex.java
@@ -0,0 +1,22 @@
+package Day17.HackerRank;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class JavaIPRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+
+    public static final String pattern = "^(([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])$";
+}
