d9503cd3050054e16a5c3b3f4939e4f3cca83afa
Shivam-Mishra-0 <cy.shivam.m@gmail.com>
2026-08-24 22:33:10 +0530

    [Shivam] : Day17 - RegexEg1, classwork problem is completed

--- Java DME/src/Day17/classwork/RegexEg1.java
+++ Java DME/src/Day17/classwork/RegexEg1.java
@@ -1,4 +1,31 @@
 package Day17.classwork;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexEg1 {
+    public static void main(String[] args) {
+        String text = "shivammishra@gmail.com";
+        String regex = "[a-zA-Z0-9.-]+@[a-z]+.[a-z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+//        if(text.matches(regex)) {
+//            System.out.println("Matched");
+//        }
+
+//        String replaceString = matcher.replaceAll("*");
+//        System.out.println(replaceString);
+//
+//        String[] splits = text.split("\\s");
+//        for(String element : splits){
+//            System.out.println(element);
+
+
+        while(matcher.find()){
+            System.out.println("Matched");
+        }
+
+    }
 }
